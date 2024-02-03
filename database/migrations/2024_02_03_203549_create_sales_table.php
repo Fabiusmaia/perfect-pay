563a09e1d77e124471e1f9bae5c15a1e059c31b4
fabiusfernandes <ffam-20@hotmail.com>
2024-02-03 17:39:33 -0300

    feat: create migrations

---
+++ database/migrations/2024_02_03_203549_create_sales_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('price');
+            $table->dateTime('date');
+            $table->smallInteger('quantity');
+            $table->string('status', 20);
+            $table->integer('discount');
+            $table->foreignId('client_id')->references('id')->on('clients')->onDelete('cascade');
+            $table->foreignId('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+};
